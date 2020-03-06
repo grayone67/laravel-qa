1fb3bc23cc161a080e4f2c532edfbbe8d18826da
grayone67 <gray.hampson@outlook.com>
2020-03-06 13:13:26 +0000

    Generate Some Seed Data

--- app/Question.php
+++ app/Question.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    protected $fillable = ['title', 'body'];
+    protected $fillable = ['title', 'body', 'user_id'];
     
     public function user() {
         return $this->belongsTo(User::class);
