4fbca539acd47979c1f0ad888a313177b58111f8
grayone67 <gray.hampson@outlook.com>
2020-03-09 10:10:55 +0000

    Changing for answers to answers count

---
+++ app/Answer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    public function question() {
+
+        return $this->belongsTo(Question::class);
+
+    }
+
+
+
+    public function user() {
+
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        $markdown = new CommonMarkConverter(['allow_unsafe_links' => false]);
+
+        return $markdown->convertToHtml($this->body);
+
+    }
+
+
+
+}
--- app/User.php
+++ app/User.php
@@ -44,4 +44,11 @@ class User extends Authenticatable
 
     }
 
+
+    public function answers() {
+
+        return $this->hasMany(Answer::class);
+
+    }
+
 }
